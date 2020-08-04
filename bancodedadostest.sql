1ffd9a9f5503c67f8637c7e94929ebac43b0200e
guirocha <guilhermerocha008@hotmail.com>
2020-08-04 18:12:32 -0300

    banco de dados salvo novo

---
+++ bancodedadostest.sql
@@ -0,0 +1,91 @@
+CREATE DATABASE  IF NOT EXISTS `test` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `test`;
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: test
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `teste`
+--
+
+DROP TABLE IF EXISTS `teste`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `teste` (
+  `id` int NOT NULL DEFAULT (_utf8mb4' '),
+  `nam` varchar(30) DEFAULT 'name',
+  `sexo` enum('M','F') DEFAULT NULL,
+  `idade` int DEFAULT NULL,
+  `profissao` varchar(20) DEFAULT NULL,
+  `altura` decimal(3,2) DEFAULT NULL,
+  `peso` int DEFAULT NULL,
+  `dia` int DEFAULT NULL,
+  `data` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `teste`
+--
+
+LOCK TABLES `teste` WRITE;
+/*!40000 ALTER TABLE `teste` DISABLE KEYS */;
+INSERT INTO `teste` VALUES (1,'guilherme','M',30,'bancario',1.81,88,NULL,''),(2,'joao','M',31,'medico',1.71,78,NULL,''),(3,'gabriel','M',22,'dentista',1.85,102,NULL,''),(4,'name','M',30,'design',1.80,88,NULL,'30');
+/*!40000 ALTER TABLE `teste` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `times1`
+--
+
+DROP TABLE IF EXISTS `times1`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `times1` (
+  `time` varchar(20) DEFAULT NULL,
+  `jogador` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `times1`
+--
+
+LOCK TABLES `times1` WRITE;
+/*!40000 ALTER TABLE `times1` DISABLE KEYS */;
+INSERT INTO `times1` VALUES ('palmeiras','palmeiras'),('oseas','palmeiras'),('palmeiras','palmeiras');
+/*!40000 ALTER TABLE `times1` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'test'
+--
+
+--
+-- Dumping routines for database 'test'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-08-04 18:11:49
